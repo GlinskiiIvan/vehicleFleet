3cbb79b58987056a5a672afd188ef14025a4a897
Glinsky Ivan <glinsky_ivan@mail.ru>
2021-11-22 23:57:11 +0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace vehicleFleet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.category = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.count = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(87, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(343, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Съездить в рейс";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.category,
+            this.count,
+            this.cost});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.Size = new System.Drawing.Size(477, 163);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // category
+            // 
+            this.category.HeaderText = "Категория";
+            this.category.Name = "category";
+            this.category.Width = 119;
+            // 
+            // count
+            // 
+            this.count.HeaderText = "Количество";
+            this.count.Name = "count";
+            this.count.Width = 130;
+            // 
+            // cost
+            // 
+            this.cost.HeaderText = "стоимость";
+            this.cost.Name = "cost";
+            this.cost.Width = 120;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 232);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn category;
+        private System.Windows.Forms.DataGridViewTextBoxColumn count;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cost;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vehicleFleet
+{
+    public partial class Form1 : Form
+    {
+
+        public class Passenger
+        {
+            private string category;
+            private int cost;
+
+            private Dictionary<string, int> categories = new Dictionary<string, int> {
+                {"children", 0},
+                {"schoolboy", 45},
+                {"pensioner", 50},
+                {"ordinary passengers", 90}
+            };
+
+            public Passenger()
+            {
+                Random random = new Random();
+                int r = random.Next(0, 4);
+                int i = 0;
+                foreach (var item in categories)
+                {
+                    if (r == i)
+                    {
+                        this.category = item.Key;
+                        this.cost = item.Value;
+                        break;
+                    }
+                    ++i;
+                }
+            }
+
+            public Passenger(string category, int cost)
+            {
+                this.category = category;
+                this.cost = cost;
+            }
+
+            public string Category
+            {
+                get { return this.category; }
+                set { this.category = value; }
+            }
+
+            public int Cost
+            {
+                get { return this.cost; }
+                set { this.cost = value; }
+            }
+
+            public Dictionary<string, int> Categories { get { return this.categories; } }
+        }
+
+        public class Bus
+        {
+            List<Passenger> passengers = new List<Passenger>();
+
+            private int Childrens = 0;
+            private int Schoolboys = 0;
+            private int Pensioners = 0;
+            private int OrdinaryPassengers = 0;
+
+            public Bus() { createPassengers(); }
+
+            public int AllChildren { get { return Childrens; } }
+            public int AllSchoolboy {  get { return Schoolboys; } }
+            public int AllPensioner {  get { return Pensioners; } }
+            public int AllOrdinaryPassenger { get { return OrdinaryPassengers; } }
+
+            public int paymentChildren() { return Childrens * passengers[0].Categories["children"]; }
+            public int paymentSchoolboys() { return Schoolboys * passengers[0].Categories["schoolboy"]; }
+            public int paymentPensioners() { return Pensioners * passengers[0].Categories["pensioner"]; }
+            public int paymentOrdinaryPassengers() { return OrdinaryPassengers * passengers[0].Categories["ordinary passengers"]; }
+            public int paymentAllPassengers()
+            {
+                return paymentChildren() + paymentSchoolboys() + paymentPensioners() + paymentOrdinaryPassengers();
+            }
+            public int numberAllPassengers() { return passengers.Count; }
+
+            public void createPassengers()
+            {
+                Random random = new Random();
+                int count = random.Next(0, 101);
+
+                for (int i = 0; i < count; ++i)
+                {
+                    Passenger passenger = new Passenger();
+                    System.Threading.Thread.Sleep(1);
+                    switch (passenger.Category)
+                    {
+                        case "children":
+                            Childrens++;
+                            break;
+                        case "schoolboy":
+                            Schoolboys++;
+                            break;
+                        case "pensioner":
+                            Pensioners++;
+                            break;
+                        case "ordinary passengers":
+                            OrdinaryPassengers++;
+                            break;
+
+                    }
+                    passengers.Add(passenger);
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Rows.Add("Дети");
+            dataGridView1.Rows.Add("Школьники");
+            dataGridView1.Rows.Add("Пенсионеры");
+            dataGridView1.Rows.Add("Обычные пассажиры");
+            dataGridView1.Rows.Add("Все категории");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bus bus = new Bus();
+
+            dataGridView1.Rows[0].Cells[1].Value = bus.AllChildren;
+            dataGridView1.Rows[0].Cells[2].Value = bus.paymentChildren() + " тг";
+
+            dataGridView1.Rows[1].Cells[1].Value = bus.AllSchoolboy;
+            dataGridView1.Rows[1].Cells[2].Value = bus.paymentSchoolboys() + " тг";
+
+            dataGridView1.Rows[2].Cells[1].Value = bus.AllPensioner;
+            dataGridView1.Rows[2].Cells[2].Value = bus.paymentPensioners() + " тг";
+
+            dataGridView1.Rows[3].Cells[1].Value = bus.AllOrdinaryPassenger;
+            dataGridView1.Rows[3].Cells[2].Value = bus.paymentOrdinaryPassengers() + " тг";
+
+            dataGridView1.Rows[4].Cells[1].Value = bus.numberAllPassengers();
+            dataGridView1.Rows[4].Cells[2].Value = bus.paymentAllPassengers() + " тг";
+        }
+    }
+}
